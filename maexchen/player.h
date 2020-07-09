58d9f01ff7c9a4ff9f3c208b0c6a5a3858b11672
MaSter0604 <steinhauser.maik@gmail.com>
2020-07-09 23:34:47 +0200

    add setGet Aussage Player.h

--- maexchen/player.h
+++ maexchen/player.h
@@ -7,6 +7,8 @@ public:
 	std::string getName() { return name; }
 	void setName(std::string name) { this->name = name; }
 	int wuerfeln();
+	bool getAussage() { return aussage; }
+	void setAussage(bool aussage) { this->aussage = aussage; }
 private:
 	std::string name = "";
 	int nPoints = 0;
